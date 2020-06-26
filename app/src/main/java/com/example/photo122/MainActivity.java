5d06fb8e7ade9bbacce592031393980d2987237c
Polina Svet <polecika1@gmail.com>
2020-06-26 10:10:16 +0300

    Initial commit

---
+++ app/src/main/java/com/example/photo122/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.photo122;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.net.URI;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button buttonBack = (Button) findViewById(R.id.button_back);
+        Button buttonNext = (Button) findViewById(R.id.button_next);
+        final TextView imageLink = (TextView) findViewById(R.id.image_link);
+        final ImageView photo = (ImageView) findViewById(R.id.photo);
+        final Random random = new Random();
+        int[] imgArray = new int[]{
+                R.drawable.photo1,
+                R.drawable.photo2,
+                R.drawable.photo3,
+                R.drawable.photo4,
+                R.drawable.photo5,
+        };
+
+        int randomInt =  random.nextInt(4);
+        photo.setImageResource(imgArray[randomInt]);
+        randomInt++;
+        imageLink.setText("drawable/photo" + (randomInt) + ".jpg");
+
+        buttonBack.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+               MainActivity.super.finish();
+            }
+        });
+
+        buttonNext.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,MainActivity.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+}
